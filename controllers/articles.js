03b6ec7de144be0bca143ab1a01d49398163d5ee
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-11-14 20:23:26 +0300

    feature/refactor

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,50 +1,57 @@
 const Article = require('../models/article');
 const BadRequestError = require('../errors/BadRequestError');
-const ServerError = require('../errors/ServerError');
 const NotFoundError = require('../errors/NotFoundError');
 const ForbiddenError = require('../errors/ForbiddenError');
 
 const getArticles = (req, res, next) => {
-  Article.find({})
-    .populate('user')
-    .then((articles) => res.status(200).send({ data: articles }))
-    .catch((err) => {
-      throw new ServerError({ message: `На сервере произошла ошибка: ${err.message}` });
+  Article.find({ owner: req.user._id })
+    .then((article) => {
+      if (article.length === 0) {
+        throw new NotFoundError('Статьи не найдены');
+      }
+      res.send({ data: article });
     })
-    .catch(next);
+    .catch((err) => {
+      if (err.kind === 'ObjectId') {
+        next(new BadRequestError('Неверный id пользователя'));
+      }
+      next(err);
+    });
 };
 
 const createArticle = (req, res, next) => {
   const {
     keyword, title, text, date, source, link, image,
   } = req.body;
-
   Article.create({
     keyword, title, text, date, source, link, image, owner: req.user._id,
   })
-    .catch((err) => {
-      throw new BadRequestError({ message: `Указаны некорректные данные при создании темы: ${err.message}` });
-    })
-    .then((article) => res.status(201).send({ data: article }))
-    .catch(next);
+    .then((result) => res.status(201).send({ data: result }))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        next(new BadRequestError(error.message));
+      }
+      next(error);
+    });
 };
 
 const deleteArticle = (req, res, next) => {
-  Article.findById(req.params._id)
-    .select('+owner')
-    .orFail()
-    .catch(() => {
-      throw new NotFoundError({ message: 'Нет темы с таким id' });
-    })
+  Article.findById(req.params.articleId).select('+owner')
     .then((article) => {
-      if (article.owner.toString() !== req.user._id) {
-        throw new ForbiddenError({ message: 'Недостаточно прав для выполнения операции' });
+      if (!article) {
+        throw new NotFoundError('Статья не найдена');
+      } else if (article.owner.toString() !== req.user._id) {
+        throw new ForbiddenError('Недостаточно прав для выполнения операции');
+      } else {
+        Article.findByIdAndRemove(req.params.articleId)
+          .then((result) => {
+            if (!result) {
+              throw new NotFoundError('Нет такой статьи');
+            }
+            res.send({ data: result });
+          })
+          .catch(next);
       }
-      Article.findByIdAndDelete(req.params._id)
-        .then((articleData) => {
-          res.send({ data: articleData });
-        })
-        .catch(next);
     })
     .catch(next);
 };
