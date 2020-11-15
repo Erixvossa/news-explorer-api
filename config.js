f2c3c390ec1dd38a77ee0c02a873cd72623954d3
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-11-15 22:32:42 +0300

    domain

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const { PORT = 3000, JWT_SECRET = 'eb6b2f657e94c7b2ecbfea1699c71a1010294d84cd257efd0b16de787635af41' } = process.env;
+const { PORT = 3000, JWT_SECRET = 'JWT_SECRET' } = process.env;
 
 module.exports = {
   PORT,
